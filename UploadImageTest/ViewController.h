6cf3bad224d0ba66090d534b9579e53fffe0a582
ShawnChen <chengchao0311@gmail.com>
2015-08-26 18:58:00 +0800

    Initial Commit

---
+++ UploadImageTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UploadImageTest
+//
+//  Created by ShawnChen on 8/26/15.
+//  Copyright (c) 2015 ShawnChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
